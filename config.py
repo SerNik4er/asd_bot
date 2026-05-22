686bdf8d180598518d8355a141189d78f0095589
Chernenko Sergey <106673431+SerNik4er@users.noreply.github.com>
2026-05-22 18:23:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "autism_helper.db")
 
+ADMIN_IDS = [1249686924, ]
 # Список полезных советов (для рассылки или команды /tip)
 TIPS = [
     "Старайтесь соблюдать режим дня — это снижает тревожность.",
