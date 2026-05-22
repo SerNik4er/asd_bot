0f9ab57dd678b3b8db447a577e9891d9d2442faa
Chernenko Sergey <106673431+SerNik4er@users.noreply.github.com>
2026-05-22 03:31:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_NAME = os.getenv("DATAVASE_NAME", "autism_helper.db")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "autism_helper.db")
 if not BOT_TOKEN:
     raise ValueError("Токен не найден! Проверьте файл .env")
